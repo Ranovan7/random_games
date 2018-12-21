e1baeac34fbab615c9e21878969d22bda6753371
rmyrnvn <romyranovan@gmail.com>
2018-12-21 10:14:31 +0700

    looping the game, adding gold tracker, adding straight check, etc

--- singlePoker/poker.py
+++ singlePoker/poker.py
@@ -10,13 +10,31 @@ if __name__ == "__main__":
 
     player = Player(name, 100)
     print(f"Player : {player}")
+    is_stop = False
     game_num = 0
     while True:
-        print(f"Game Number {game_num+1}\n")
-        game = Game(player, Deck())
-
+        print(f"GAME NUMBER {game_num+1}\n")
+        game = Game(player, Deck(), False)
         game.start()
         game.play()
-
         game_num += 1
-        break
+
+        if player.money < 3:
+            print("Your Gold is too low, you cannot play anymore")
+            break
+
+        print("Play again? (y/n)")
+        while True:
+            rematch = input()
+            if rematch == 'y':
+                print("Next game")
+                break
+            elif rematch == 'n':
+                print("Thanks for Playing, have a good day.")
+                is_stop = True
+                break
+            else:
+                print("Invalid Input!")
+        print()
+        if is_stop:
+            break
--- singlePoker/table.py
+++ singlePoker/table.py
@@ -3,20 +3,24 @@ from random import randrange
 
 class Game:
 
-    def __init__(self, player, deck):
+    def __init__(self, player, deck, logging=False):
+        '''Initialize the game'''
         self.player = player
-        self.bet_amount = randrange(round(player.money * 0.1), round(player.money * 0.4))
+        self.bet_amount = max(1, randrange(round(player.money * 0.05), round(player.money * 0.3)))
         self.deck = deck
         self.pool = 0
+        self.logging = logging
 
     def __str__(self):
         return f"Poker Game for {self.player.name} with {len(self.deck)} remaining cards on deck."
 
     def call(self):
+        '''Remove players gold equal to bet amount'''
         self.player.bet(self.bet_amount)
         self.pool += (self.bet_amount*2)
 
     def start(self):
+        '''Starting the game'''
         self.deck.shuffle()
         self.open_cards = []
         self.player_cards = []
@@ -24,27 +28,35 @@ class Game:
         for i in range(2):
             self.player_cards.append(self.deck.draw())
             self.server.append(self.deck.draw())
-        for i in range(5):
+        for i in range(3):
             self.open_cards.append(self.deck.draw())
+        self.call()
 
     def show(self):
+        '''Show the shared cards on the table'''
+        print(f"Betted Gold : {self.pool}")
+        print("Cards on the Table")
         for card in self.open_cards:
             print(f"- {card}")
         print()
 
     def show_player_cards(self):
-        print("Your Cards :")
+        '''Show players cards'''
+        self.player.current_gold()
+        print("Players Cards :")
         for card in self.player_cards:
             print(f"- {card}")
         print()
 
     def show_server_cards(self):
+        '''Show servers cards (Debug Only)'''
         print("Server Cards :")
         for card in self.server:
             print(f"- {card}")
         print()
 
     def update_info(self, card, info):
+        '''Assessing the strength of the cards '''
         if card.type == -1:
             info['joker'] = True
             return
@@ -60,16 +72,17 @@ class Game:
             info['type'][card.type] += 1
 
     def check_combination(self, info):
+        '''From cards  strength, calculate the possible combination'''
         # this function still does not count the joker
         combination = [
             ["Have Pair", False],
             ["Have double Pairs", False],
             ["Have Tres", False],
-            ["Have a Straight", False],
+            ["Have a Straight", False],# not done
             ["Have a Flush", False],
             ["Have Full House", False],
             ["Have Four of a Kind", False],
-            ["Have a Straight-Flush", False],
+            ["Have a Straight-Flush", False],# not done
             ["Have Five of a Kind", False],
         ]
         # check pair or double pair, and 4 and 5 of a kind
@@ -106,6 +119,23 @@ class Game:
             if info['type'][i] >= 5:
                 combination[4][1] = True
 
+        # check straight and straight-flush
+        info['rank'] = sorted(info['rank'])
+        count = 0
+        curr = None
+        for i in info['rank']:
+            if count == 0:
+                count += 1
+                curr = i
+                continue
+            if curr == (i - 1):
+                count += 1
+            else:
+                count = 1
+        if count >= 5:
+            # straight true
+            combination[3][1] = True
+
         result = {
             'message': "This ain't got nothing",
             'value': -1
@@ -117,6 +147,7 @@ class Game:
         return result
 
     def check(self):
+        '''End of the game, Calculate and Compare the strength of players and servers cards'''
         player_info = {
             'type': {},
             'rank': {},
@@ -137,13 +168,18 @@ class Game:
 
         Sresult = self.check_combination(server_info)
         Presult = self.check_combination(player_info)
+        print(f"Server has {Sresult['message']}")
+        print(f"Player has {Presult['message']}")
         if Sresult['value'] > Presult['value']:
             return False
         elif Sresult['value'] < Presult['value']:
             return True
         else:
+            print("Draw")
             highestP = self.highest(self.player_cards)
             highestS = self.highest(self.server)
+            print(f"Server best card {highestS}")
+            print(f"Player best card {highestP}")
             if highestP.rank > highestS.rank:
                 return True
             elif highestP.rank == highestS.rank:
@@ -155,6 +191,7 @@ class Game:
                 return False
 
     def highest(self, cards):
+        '''Determine the strongest card in each player and server hands'''
         highest = cards[0]
         for card in cards[1:]:
             if card.rank > highest.rank:
@@ -162,34 +199,51 @@ class Game:
             elif card.rank == highest.rank:
                 if card.type > highest.type:
                     highest = card
-        print(highest)
         return highest
 
-    def show_current(self):
-        self.show_server_cards()
+    def add_on_table(self):
+        '''Adding one card to shared cards on the table'''
+        self.open_cards.append(self.deck.draw())
+
+    def show_current(self, end=False):
+        '''Showing the current state of the game'''
+        if self.logging or end:
+            self.show_server_cards()
         self.show()
         self.show_player_cards()
 
     def play(self):
+        '''Rundown play of the game'''
         called = True
-        for i in range(3):
+        for i in range(2):
             self.show_current()
             print("Call (c) or Fold (f)?")
-            is_call = input()
-            if is_call == 'c':
-                continue
-            elif is_call == 'f':
-                print("Player Folded.")
-                called = False
+            while True:
+                is_call = input()
+                print()
+                if is_call == 'c':
+                    self.add_on_table()
+                    self.call()
+                    break
+                elif is_call == 'f':
+                    print("Player Folded.")
+                    called = False
+                    break
+                else:
+                    print("Invalid Input!")
+            if called is False:
                 break
-            else:
-                print("Invalid Input!")
+
         if called:
+            self.show_current(True)
             winner = self.check()
             if winner:
+                self.player.won(self.pool)
                 print("Player Won the Game")
+                print(f"Players Gold has increased to {self.player.money} gold")
             else:
                 print("Player Lose, Server Won the Game")
+                print(f"Players Gold has reduced to {self.player.money} gold")
         else:
             print("You Chicken!")
 
